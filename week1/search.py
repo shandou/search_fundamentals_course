9980b146b9c48f22946484c9b88ec5042592fe13
Shan Dou <shandou.seismo@gmail.com>
2022-06-19 01:43:12 +0000

    (WIP) query composition

--- week1/search.py
+++ week1/search.py
@@ -1,13 +1,11 @@
 #
 # The main search hooks for the Search Flask application.
 #
-from flask import (
-    Blueprint, redirect, render_template, request, url_for
-)
+from flask import Blueprint, redirect, render_template, request, url_for
 
 from week1.opensearch import get_opensearch
 
-bp = Blueprint('search', __name__, url_prefix='/search')
+bp = Blueprint("search", __name__, url_prefix="/search")
 
 
 # Process the filters requested by the user and return a tuple that is appropriate for use in: the query, URLs displaying the filter and the display of the applied filters
@@ -22,8 +20,9 @@ def process_filters(filters_input):
     for filter in filters_input:
         type = request.args.get(filter + ".type")
         display_name = request.args.get(filter + ".displayName", filter)
-        applied_filters += "&filter.name={}&{}.type={}&{}.displayName={}".format(filter, filter, type, filter,
-                                                                                 display_name)
+        applied_filters += "&filter.name={}&{}.type={}&{}.displayName={}".format(
+            filter, filter, type, filter, display_name
+        )
         if type == "range":
             from_val = request.args.get(filter + ".from", None)
             to_val = request.args.get(filter + ".to", None)
@@ -54,11 +53,10 @@ def process_filters(filters_input):
     return filters, display_filters, applied_filters
 
 
-
 # Our main query route.  Accepts POST (via the Search box) and GETs via the clicks on aggregations/facets
-@bp.route('/query', methods=['GET', 'POST'])
+@bp.route("/query", methods=["GET", "POST"])
 def query():
-    opensearch = get_opensearch() # Load up our OpenSearch client from the opensearch.py file.
+    opensearch = get_opensearch()  # Load up our OpenSearch client from the opensearch.py file.
     # Put in your code to query opensearch.  Set error as appropriate.
     error = None
     user_query = None
@@ -68,8 +66,8 @@ def query():
     filters = None
     sort = "_score"
     sortDir = "desc"
-    if request.method == 'POST':  # a query has been submitted
-        user_query = request.form['query']
+    if request.method == "POST":  # a query has been submitted
+        user_query = request.form["query"]
         if not user_query:
             user_query = "*"
         sort = request.form["sort"]
@@ -79,7 +77,9 @@ def query():
         if not sortDir:
             sortDir = "desc"
         query_obj = create_query(user_query, [], sort, sortDir)
-    elif request.method == 'GET':  # Handle the case where there is no query or just loading the page
+    elif (
+        request.method == "GET"
+    ):  # Handle the case where there is no query or just loading the page
         user_query = request.args.get("query", "*")
         filters_input = request.args.getlist("filter.name")
         sort = request.args.get("sort", sort)
@@ -94,14 +94,20 @@ def query():
     print("query obj: {}".format(query_obj))
 
     #### Step 4.b.ii
-    response = None   # TODO: Replace me with an appropriate call to OpenSearch
+    response = None  # TODO: Replace me with an appropriate call to OpenSearch
     # Postprocess results here if you so desire
 
-    #print(response)
+    # print(response)
     if error is None:
-        return render_template("search_results.jinja2", query=user_query, search_response=response,
-                               display_filters=display_filters, applied_filters=applied_filters,
-                               sort=sort, sortDir=sortDir)
+        return render_template(
+            "search_results.jinja2",
+            query=user_query,
+            search_response=response,
+            display_filters=display_filters,
+            applied_filters=applied_filters,
+            sort=sort,
+            sortDir=sortDir,
+        )
     else:
         redirect(url_for("index"))
 
@@ -109,13 +115,21 @@ def query():
 def create_query(user_query, filters, sort="_score", sortDir="desc"):
     print("Query: {} Filters: {} Sort: {}".format(user_query, filters, sort))
     query_obj = {
-        'size': 10,
+        "size": 10,
         "query": {
-            "match_all": {} # Replace me with a query that both searches and filters
+            "bool": {
+                "must": [
+                    {
+                        "query_string": {
+                            "query": user_query,
+                            "fields": ["name", "shortDescription", "longDescription"],
+                            "phrase_slop": 3,
+                        }
+                    }
+                ],
+                "filter": filters,
+            },
+            "aggs": {"regularPrice": {"field": "regularPrice", "ranges": []}},
         },
-        "aggs": {
-            #### Step 4.b.i: create the appropriate query and aggregations here
-
-        }
     }
     return query_obj
