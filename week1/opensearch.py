56a90920abc146ea56273aab4788b256e119d009
Shan Dou <shandou.seismo@gmail.com>
2022-06-19 01:42:56 +0000

    Add connection logic

--- week1/opensearch.py
+++ week1/opensearch.py
@@ -3,9 +3,15 @@ from opensearchpy import OpenSearch
 
 # Create an OpenSearch client instance and put it into Flask shared space for use by the application
 def get_opensearch():
-    if 'opensearch' not in g:
+    host = "localhost"
+    port = 9200
+    auth = ("admin", "admin")
+
+    if "opensearch" not in g:
         #### Step 4.a:
         # Implement a client connection to OpenSearch so that the rest of the application can communicate with OpenSearch
-        g.opensearch = None
+        g.opensearch = OpenSearch(
+            hosts=[{"host": host, "port": port}], http_auth=auth, scheme="https", verify_certs=False
+        )
 
     return g.opensearch
