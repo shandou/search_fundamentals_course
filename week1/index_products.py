4781e819e1c629d476388e89fec334a316660bad
Shan Dou <shandou.seismo@gmail.com>
2022-06-19 01:41:13 +0000

    indexing small amount for testing

--- week1/index_products.py
+++ week1/index_products.py
@@ -11,70 +11,108 @@ import time
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-logging.basicConfig(format='%(levelname)s:%(message)s')
+logging.basicConfig(format="%(levelname)s:%(message)s")
 
 # NOTE: this is not a complete list of fields.  If you wish to add more, put in the appropriate XPath expression.
 # TODO: is there a way to do this using XPath/XSL Functions so that we don't have to maintain a big list?
 mappings = [
-    "sku/text()", "sku", # SKU is the unique ID, productIds can have multiple skus
-    "productId/text()", "productId",
-    "name/text()", "name",
-    "type/text()", "type",
-    "regularPrice/text()", "regularPrice",
-    "salePrice/text()", "salePrice",
-    "onSale/text()", "onSale",
-    "salesRankShortTerm/text()", "salesRankShortTerm",
-    "salesRankMediumTerm/text()", "salesRankMediumTerm",
-    "salesRankLongTerm/text()", "salesRankLongTerm",
-    "bestSellingRank/text()", "bestSellingRank",
-    "url/text()", "url",
-    "categoryPath/*/name/text()", "categoryPath",  # Note the match all here to get the subfields
-    "categoryPath/*/id/text()", "categoryPathIds",  # Note the match all here to get the subfields
-    "categoryPath/category[last()]/id/text()", "categoryLeaf",
-    "count(categoryPath/*/name)", "categoryPathCount",
-    "customerReviewCount/text()", "customerReviewCount",
-    "customerReviewAverage/text()", "customerReviewAverage",
-    "inStoreAvailability/text()", "inStoreAvailability",
-    "onlineAvailability/text()", "onlineAvailability",
-    "releaseDate/text()", "releaseDate",
-    "shortDescription/text()", "shortDescription",
-    "class/text()", "class",
-    "classId/text()", "classId",
-    "department/text()", "department",
-    "departmentId/text()", "departmentId",
-    "bestBuyItemId/text()", "bestBuyItemId",
-    "description/text()", "description",
-    "manufacturer/text()", "manufacturer",
-    "modelNumber/text()", "modelNumber",
-    "image/text()", "image",
-    "longDescription/text()", "longDescription",
-    "longDescriptionHtml/text()", "longDescriptionHtml",
-    "features/*/text()", "features"  # Note the match all here to get the subfields
-
+    "sku/text()",
+    "sku",  # SKU is the unique ID, productIds can have multiple skus
+    "productId/text()",
+    "productId",
+    "name/text()",
+    "name",
+    "type/text()",
+    "type",
+    "regularPrice/text()",
+    "regularPrice",
+    "salePrice/text()",
+    "salePrice",
+    "onSale/text()",
+    "onSale",
+    "salesRankShortTerm/text()",
+    "salesRankShortTerm",
+    "salesRankMediumTerm/text()",
+    "salesRankMediumTerm",
+    "salesRankLongTerm/text()",
+    "salesRankLongTerm",
+    "bestSellingRank/text()",
+    "bestSellingRank",
+    "url/text()",
+    "url",
+    "categoryPath/*/name/text()",
+    "categoryPath",  # Note the match all here to get the subfields
+    "categoryPath/*/id/text()",
+    "categoryPathIds",  # Note the match all here to get the subfields
+    "categoryPath/category[last()]/id/text()",
+    "categoryLeaf",
+    "count(categoryPath/*/name)",
+    "categoryPathCount",
+    "customerReviewCount/text()",
+    "customerReviewCount",
+    "customerReviewAverage/text()",
+    "customerReviewAverage",
+    "inStoreAvailability/text()",
+    "inStoreAvailability",
+    "onlineAvailability/text()",
+    "onlineAvailability",
+    "releaseDate/text()",
+    "releaseDate",
+    "shortDescription/text()",
+    "shortDescription",
+    "class/text()",
+    "class",
+    "classId/text()",
+    "classId",
+    "department/text()",
+    "department",
+    "departmentId/text()",
+    "departmentId",
+    "bestBuyItemId/text()",
+    "bestBuyItemId",
+    "description/text()",
+    "description",
+    "manufacturer/text()",
+    "manufacturer",
+    "modelNumber/text()",
+    "modelNumber",
+    "image/text()",
+    "image",
+    "longDescription/text()",
+    "longDescription",
+    "longDescriptionHtml/text()",
+    "longDescriptionHtml",
+    "features/*/text()",
+    "features",  # Note the match all here to get the subfields
 ]
 
 
 def get_opensearch():
-    host = 'localhost'
+    host = "localhost"
     port = 9200
-    auth = ('admin', 'admin')
+    auth = ("admin", "admin")
 
     #### Step 2.a: Create a connection to OpenSearch
-    client = None
+    client = OpenSearch(
+        hosts=[{"host": host, "port": port}], http_auth=auth, scheme="https", verify_certs=False
+    )
     return client
 
 
 @click.command()
-@click.option('--source_dir', '-s', help='XML files source directory')
-@click.option('--index_name', '-i', default="bbuy_products", help="The name of the index to write to")
+@click.option("--source_dir", "-s", help="XML files source directory")
+@click.option(
+    "--index_name", "-i", default="bbuy_products", help="The name of the index to write to"
+)
 def main(source_dir: str, index_name: str):
     client = get_opensearch()
     # To test on a smaller set of documents, change this glob to be more restrictive than *.xml
-    files = glob.glob(source_dir + "/*.xml")
+    # files = glob.glob(source_dir + "/*.xml")
+    files = glob.glob(source_dir + "/products_000[12]_*.xml")
     docs_indexed = 0
     tic = time.perf_counter()
     for file in files:
-        logger.info(f'Processing file : {file}')
+        logger.info(f"Processing file : {file}")
         tree = etree.parse(file)
         root = tree.getroot()
         children = root.findall("./product")
@@ -86,14 +124,19 @@ def main(source_dir: str, index_name: str):
                 key = mappings[idx + 1]
                 doc[key] = child.xpath(xpath_expr)
             # print(doc)
-            if not 'productId' in doc or len(doc['productId']) == 0:
+            if not "productId" in doc or len(doc["productId"]) == 0:
                 continue
 
             #### Step 2.b: Create a valid OpenSearch Doc and bulk index 2000 docs at a time
-            the_doc = None
+            the_doc = {"_id": docs_indexed, "_source": doc}
+            docs_indexed += 1
             docs.append(the_doc)
+            if len(docs) == 2000:
+                bulk(client=client, actions=docs, index=index_name)
+                docs = []
+        bulk(client=client, actions=docs, index=index_name)
     toc = time.perf_counter()
-    logger.info(f'Done. Total docs: {docs_indexed}.  Total time: {((toc - tic) / 60):0.3f} mins.')
+    logger.info(f"Done. Total docs: {docs_indexed}.  Total time: {((toc - tic) / 60):0.3f} mins.")
 
 
 if __name__ == "__main__":
